1bdd57c0fc1e0369b597c5217c132b59950a6917
Tobe Okafor <teokafor@gmail.com>
2025-01-21 21:30:28 -0600

    Implement drawer rendering

--- src/renderController.js
+++ src/renderController.js
@@ -23,9 +23,30 @@ function drawProjects() {
 }
 
 function drawProject(projectName) {
-    // let drawer = document.createElement('div');
-    // drawer.classList.add('project-drawer');
-    // content.appendChild(drawer);
+    let drawer = document.createElement('div');
+    drawer.classList.add('project-drawer');
+
+    for (let todo in json[projectName]) {
+        let todoItem = document.createElement('div');
+
+        todoItem.innerHTML = 
+        `<div class="todo-shell">
+            <div class="todo-left">
+                <div>${json[projectName][todo].title}</div>
+                <div>${json[projectName][todo].desc}</div>
+            </div>
+
+            <div class="todo-right">
+                <div>Created: 2022-10-07</div>
+                <div class="todo-buttons">
+                    <div>YeaButton</div>
+                    <div>NayButton</div>
+                </div>
+            </div>
+        </div>`;
+        drawer.appendChild(todoItem);
+        document.body.appendChild(drawer);
+    }
 }
 
 
